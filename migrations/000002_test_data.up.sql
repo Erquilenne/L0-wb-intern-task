2f08f410608865ea42e15a88b32d4fc176183436
user <user@ubuntu.myguest.virtualbox.org>
2024-01-21 23:21:46 +0300

    test data

---
+++ migrations/000002_test_data.up.sql
@@ -0,0 +1,92 @@
+-- Добавление тестовых элементов в orders
+INSERT INTO orders (order_uid, track_number, entry, delivery_name, delivery_phone, delivery_zip, delivery_city, delivery_address, delivery_region, delivery_email, payment_transaction, payment_request_id, payment_currency, payment_provider, payment_amount, payment_dt, payment_bank, payment_delivery_cost, payment_goods_total, payment_custom_fee, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+VALUES
+('order_uid_1', 'track_number_1', 'entry_1', 'Test Name 1', '+1234567890', '12345', 'City 1', 'Address 1', 'Region 1', 'test1@example.com', 'transaction_1', 'request_id_1', 'USD', 'provider_1', 100, NOW(), 'bank_1', 10, 90, 5, 'en', 'internal_signature_1', 'customer_id_1', 'delivery_service_1', 'shardkey_1', 1, NOW(), 'oof_shard_1');
+INSERT INTO orders (order_uid, track_number, entry, delivery_name, delivery_phone, delivery_zip, delivery_city, delivery_address, delivery_region, delivery_email, payment_transaction, payment_request_id, payment_currency, payment_provider, payment_amount, payment_dt, payment_bank, payment_delivery_cost, payment_goods_total, payment_custom_fee, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+VALUES
+('order_uid_2', 'track_number_2', 'entry_2', 'Test Name 2', '+2345678901', '23456', 'City 2', 'Address 2', 'Region 2', 'test2@example.com', 'transaction_2', 'request_id_2', 'USD', 'provider_2', 200, NOW(), 'bank_2', 20, 180, 10, 'en', 'internal_signature_2', 'customer_id_2', 'delivery_service_2', 'shardkey_2', 2, NOW(), 'oof_shard_2');
+INSERT INTO orders (order_uid, track_number, entry, delivery_name, delivery_phone, delivery_zip, delivery_city, delivery_address, delivery_region, delivery_email, payment_transaction, payment_request_id, payment_currency, payment_provider, payment_amount, payment_dt, payment_bank, payment_delivery_cost, payment_goods_total, payment_custom_fee, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+VALUES
+('order_uid_3', 'track_number_3', 'entry_3', 'Test Name 3', '+1234563890', '12315', 'City 3', 'Address 3', 'Region 3', 'test3@example.com', 'transaction_3', 'request_id_3', 'USD', 'provider_3', 300, NOW(), 'bank_3', 30, 290, 5, 'en', 'internal_signature_3', 'customer_id_3', 'delivery_service_1', 'shardkey_3', 3, NOW(), 'oof_shard_1');
+INSERT INTO orders (order_uid, track_number, entry, delivery_name, delivery_phone, delivery_zip, delivery_city, delivery_address, delivery_region, delivery_email, payment_transaction, payment_request_id, payment_currency, payment_provider, payment_amount, payment_dt, payment_bank, payment_delivery_cost, payment_goods_total, payment_custom_fee, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+VALUES
+('order_uid_4', 'track_number_4', 'entry_4', 'Test Name 4', '+2345674901', '23426', 'City 4', 'Address 4', 'Region 4', 'test4@example.com', 'transaction_4', 'request_id_4', 'USD', 'provider_4', 400, NOW(), 'bank_4', 40, 3180, 10, 'en', 'internal_signature_4', 'customer_id_4', 'delivery_service_2', 'shardkey_4', 4, NOW(), 'oof_shard_2');
+INSERT INTO orders (order_uid, track_number, entry, delivery_name, delivery_phone, delivery_zip, delivery_city, delivery_address, delivery_region, delivery_email, payment_transaction, payment_request_id, payment_currency, payment_provider, payment_amount, payment_dt, payment_bank, payment_delivery_cost, payment_goods_total, payment_custom_fee, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+VALUES
+('order_uid_5', 'track_number_5', 'entry_5', 'Test Name 5', '+1234565890', '12335', 'City 5', 'Address 5', 'Region 5', 'test5@example.com', 'transaction_5', 'request_id_5', 'USD', 'provider_5', 500, NOW(), 'bank_5', 50, 490, 5, 'en', 'internal_signature_5', 'customer_id_5', 'delivery_service_1', 'shardkey_5', 5, NOW(), 'oof_shard_1');
+INSERT INTO orders (order_uid, track_number, entry, delivery_name, delivery_phone, delivery_zip, delivery_city, delivery_address, delivery_region, delivery_email, payment_transaction, payment_request_id, payment_currency, payment_provider, payment_amount, payment_dt, payment_bank, payment_delivery_cost, payment_goods_total, payment_custom_fee, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+VALUES
+('order_uid_6', 'track_number_6', 'entry_6', 'Test Name 6', '+2345676901', '23446', 'City 6', 'Address 6', 'Region 6', 'test6@example.com', 'transaction_6', 'request_id_6', 'USD', 'provider_6', 600, NOW(), 'bank_6', 60, 5180, 10, 'en', 'internal_signature_6', 'customer_id_6', 'delivery_service_2', 'shardkey_6', 6, NOW(), 'oof_shard_2');
+INSERT INTO orders (order_uid, track_number, entry, delivery_name, delivery_phone, delivery_zip, delivery_city, delivery_address, delivery_region, delivery_email, payment_transaction, payment_request_id, payment_currency, payment_provider, payment_amount, payment_dt, payment_bank, payment_delivery_cost, payment_goods_total, payment_custom_fee, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+VALUES
+('order_uid_7', 'track_number_7', 'entry_7', 'Test Name 7', '+1234567890', '12355', 'City 7', 'Address 7', 'Region 7', 'test7@example.com', 'transaction_7', 'request_id_7', 'USD', 'provider_7', 700, NOW(), 'bank_7', 70, 690, 5, 'en', 'internal_signature_7', 'customer_id_7', 'delivery_service_1', 'shardkey_7', 7, NOW(), 'oof_shard_1');
+INSERT INTO orders (order_uid, track_number, entry, delivery_name, delivery_phone, delivery_zip, delivery_city, delivery_address, delivery_region, delivery_email, payment_transaction, payment_request_id, payment_currency, payment_provider, payment_amount, payment_dt, payment_bank, payment_delivery_cost, payment_goods_total, payment_custom_fee, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+VALUES
+('order_uid_8', 'track_number_8', 'entry_8', 'Test Name 8', '+2345672901', '23466', 'City 8', 'Address 8', 'Region 8', 'test8@example.com', 'transaction_8', 'request_id_8', 'USD', 'provider_8', 800, NOW(), 'bank_8', 80, 7180, 10, 'en', 'internal_signature_8', 'customer_id_8', 'delivery_service_2', 'shardkey_8', 8, NOW(), 'oof_shard_2');
+
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(1, 1, 'track_number_1', 50, 'rid_1', 'Item 1', 5, 'Size 1', 50, 1, 'Brand 1', 1);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(1, 2, 'track_number_1', 60, 'rid_2', 'Item 2', 6, 'Size 2', 60, 2, 'Brand 2', 2);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(1, 3, 'track_number_1', 70, 'rid_3', 'Item 3', 7, 'Size 3', 70, 3, 'Brand 3', 3);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(1, 4, 'track_number_1', 80, 'rid_4', 'Item 4', 8, 'Size 4', 80, 4, 'Brand 4', 4);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(1, 5, 'track_number_1', 90, 'rid_5', 'Item 5', 9, 'Size 5', 90, 5, 'Brand 5', 5);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(2, 6, 'track_number_2', 110, 'rid_6', 'Item 6', 11, 'Size 6', 110, 6, 'Brand 6', 1);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(2, 7, 'track_number_2', 120, 'rid_7', 'Item 7', 12, 'Size 7', 120, 7, 'Brand 7', 2);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(2, 8, 'track_number_2', 130, 'rid_8', 'Item 8', 13, 'Size 8', 130, 8, 'Brand 8', 3);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(2, 9, 'track_number_2', 140, 'rid_9', 'Item 9', 14, 'Size 9', 140, 9, 'Brand 9', 4);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(2, 10, 'track_number_2', 150, 'rid_10', 'Item 10', 15, 'Size 10', 150, 10, 'Brand 10', 5);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(3, 11, 'track_number_3', 90, 'rid_11', 'Item 11', 9, 'Size 11', 90, 11, 'Brand 11', 1);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(3, 12, 'track_number_3', 100, 'rid_12', 'Item 12', 10, 'Size 12', 100, 12, 'Brand 12', 2);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(4, 13, 'track_number_4', 110, 'rid_13', 'Item 13', 11, 'Size 13', 110, 13, 'Brand 13', 1);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(4, 14, 'track_number_4', 120, 'rid_14', 'Item 14', 12, 'Size 14', 120, 14, 'Brand 14', 2);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(5, 15, 'track_number_5', 130, 'rid_15', 'Item 15', 13, 'Size 15', 130, 15, 'Brand 15', 1);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(5, 16, 'track_number_5', 140, 'rid_16', 'Item 16', 14, 'Size 16', 140, 16, 'Brand 16', 2);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(6, 17, 'track_number_6', 150, 'rid_17', 'Item 17', 15, 'Size 17', 150, 17, 'Brand 17', 1);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(6, 18, 'track_number_6', 160, 'rid_18', 'Item 18', 16, 'Size 18', 160, 18, 'Brand 18', 2);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(7, 19, 'track_number_7', 170, 'rid_19', 'Item 19', 17, 'Size 19', 170, 19, 'Brand 19', 1);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(7, 20, 'track_number_7', 180, 'rid_20', 'Item 20', 18, 'Size 20', 180, 20, 'Brand 20', 2);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(8, 21, 'track_number_8', 190, 'rid_21', 'Item 21', 19, 'Size 21', 190, 21, 'Brand 21', 1);
+INSERT INTO order_items (order_id, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+VALUES
+(8, 22, 'track_number_8', 200, 'rid_22', 'Item 22', 20, 'Size 22', 200, 22, 'Brand 22', 2);
